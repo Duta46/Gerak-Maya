8e97c98a8f06cf39fdc2e2c0155b0864462f79b2
Duta <dutaalif.20091@mhs.unesa.ac.id>
2025-01-01 21:11:54 +0700

    fixing view quiz

--- resources/views/user/kuis/index.blade.php
+++ resources/views/user/kuis/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <!-- Icon -->
     <link rel="shortcut icon" href="{{ asset('assets/icon/gm.png') }}" type="image/x-icon" />
@@ -8,16 +9,13 @@
     <!-- Font Inter -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet" />
 
     <!-- Font Awesome CDN -->
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-      integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Animate Style -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
@@ -35,28 +33,66 @@
 </head>
 
 <div class="radio-input">
-  <div class="info">
-  <span class="question">What does CSS stand for?</span>
-  <span class="steps">3/10</span>
-   </div>
-  <input type="radio" id="value-1" name="value-radio" value="value-1">
-  <label for="value-1">Computer Style Sheets</label>
-  <input type="radio" id="value-2" name="value-radio" value="value-2">
-  <label for="value-2">Cascading Style Sheets</label>
-  <input type="radio" id="value-3" name="value-radio" value="value-3">
-  <label for="value-3">Creative Style Sheets</label>
-   <span class="result success">Congratulations!</span>
-   <span class="result error">Bad answer</span>
+    <div class="info">
+        <span class="question">What does CSS stand for?</span>
+        <span class="steps">3/10</span>
+    </div>
+    <div class="options">
+        <button class="option-button" onclick="selectOption(this, 'value-1')">Computer Style Sheets</button>
+        <button class="option-button" onclick="selectOption(this, 'value-2')">Cascading Style Sheets</button>
+        <button class="option-button" onclick="selectOption(this, 'value-3')">Creative Style Sheets</button>
+    </div>
+    <span class="result success">Congratulations!</span>
+    <span class="result error">Bad answer</span>
+
+    <!-- Navigation Buttons -->
+    <div class="navigation-buttons">
+        <button class="prev-button" onclick="prevQuestion()">Previous Question</button>
+        <button class="flag-button" onclick="flagQuestion()">Ragu-ragu</button>
+        <button class="next-button" onclick="nextQuestion()">Next Question</button>
+    </div>
 </div>
 
-    <!-- JS -->
-    {{-- <script src="quiz/quiz.js"></script> --}}
-    <script src="{{ asset('assets/js/quiz.js') }}"></script>
 
-    <!-- AOS -->
-    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-    <script>
-      AOS.init();
-    </script>
+
+
+<!-- JS -->
+{{-- <script src="quiz/quiz.js"></script> --}}
+<script src="{{ asset('assets/js/quiz.js') }}"></script>
+
+
+<script>
+    function selectOption(button, value) {
+    // Hapus seleksi dari tombol lain
+    document.querySelectorAll('.option-button').forEach(btn => {
+        btn.classList.remove('selected');
+    });
+
+    // Tambahkan kelas "selected" ke tombol yang dipilih
+    button.classList.add('selected');
+
+    // Simpan nilai pilihan (bisa dikirim ke server atau diproses lebih lanjut)
+    console.log("Selected value:", value);
+}
+
+function prevQuestion() {
+    alert("Navigasi ke pertanyaan sebelumnya.");
+}
+
+function flagQuestion() {
+    alert("Menandai pertanyaan sebagai ragu-ragu.");
+}
+
+function nextQuestion() {
+    alert("Navigasi ke pertanyaan berikutnya.");
+}
+
+</script>
+<!-- AOS -->
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
 </body>
+
 </html>
